d04b39173cf60d0c7e42b1c07cbb2f1fb1091662
Ben Wozak <benwozak@gmail.com>
2025-06-09 12:29:37 -0600

    clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,58 @@ import { ThemeProvider } from "@/components/layout/theme-provider.tsx";
 import NavBar from "./components/layout/NavBar.tsx";
 import PWABadge from "./PWABadge.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
 import MealPlan from "./pages/MealPlan";
 import Recipes from "./pages/Recipes";
 import Profile from "./pages/Profile";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const handler = (e: MouseEvent) => e.preventDefault();
-    document.addEventListener("contextmenu", handler);
-    return () => document.removeEventListener("contextmenu", handler);
-  }, []);
+  // useEffect(() => {
+  //   const handler = (e: MouseEvent) => e.preventDefault();
+  //   document.addEventListener("contextmenu", handler);
+  //   return () => document.removeEventListener("contextmenu", handler);
+  // }, []);
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Router>
-        <Routes>
-          <Route path="/" element={<MealPlan />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <NavBar />
-        <PWABadge />
-      </Router>
+      <header>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <SignedIn>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MealPlan />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+          <NavBar />
+          <PWABadge />
+        </Router>
+      </SignedIn>
+      <SignedOut>
+        <div className="flex flex-col h-screen items-center justify-center">
+          <h1 className="text-2xl font-bold mb-2">Sign In</h1>
+          <SignInButton>
+            <button className="bg-primary text-primary-foreground px-8 py-2 rounded hover:cursor-pointer">
+              Sign In
+            </button>
+          </SignInButton>
+          <h1 className="text-2xl font-bold my-2">Or</h1>
+          <SignUpButton>
+            <button className="border-2 border-primary border-solid text-primary px-8 py-2 rounded hover:cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </div>
+      </SignedOut>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,20 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Helmet>
-        {/* <meta
+        <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
@@ -17,9 +25,11 @@ createRoot(document.getElementById("root")!).render(
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
-        <meta name="mobile-web-app-capable" content="yes" /> */}
+        <meta name="mobile-web-app-capable" content="yes" />
       </Helmet>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
     </HelmetProvider>
   </StrictMode>
 );
